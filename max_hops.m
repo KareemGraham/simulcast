995b4fd99ba2072c3c51582d565edf8d48d7f647
kareemg <kareemg@39738bbd-366b-da62-1013-27541a91aac4>
2010-03-25 15:28:30 +0000

    made it R2008b compliant

--- max_hops.m
+++ max_hops.m
@@ -2,7 +2,7 @@ function [ mhop ] = max_hops( links )
 %finds maximum hops from a node
 %   Detailed explanation goes here
 for x=1:length(links)
-[d,~,~] = bfs(links,x,0);
+d = bfs(links,x,0);
 mhop(x) = max(d);
 end
 end
