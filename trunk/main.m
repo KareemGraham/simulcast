46bd0c1cbbf53a2d5806d42abf6fd2a9c2e78eb7
tomanishbansal <tomanishbansal@39738bbd-366b-da62-1013-27541a91aac4>
2010-03-30 06:11:02 +0000

    Introduced the Node structure to keep track of all the queues associated with it. Put new fields in the NW_Pkt type structure for bookkeeping purpose. Declared type of packets. Did node and packet initialization. Introduced separate queues for forwarding packets and local packets with subqueues for more capable links and less capable links. Fetch the packets from NW link packet array and schedule it in one of queues depending upon if there is a more capable links available. Phew!! Need to do sanity check tomorrow.

--- trunk/main.m
+++ trunk/main.m
@@ -14,6 +14,7 @@
  Xmax   = 1000;      % X dimension of area in meters
  Ymax   = 1000;      % Y dimension of area in meters
  Sig    = 0;        % STD distribution of nodes in the area
+ Node   = struct('ID', [], 'TxMCB', [], 'TxLCB', [], 'RxBuf', [], 'iNWQ', [], 'iMCQ', [], 'iLCQ', []);
  
  % Declaring Slotted ALOHA Network Parameters
  
@@ -45,27 +46,52 @@
  % NP is calculated as the attempt rate times number of slots. This should
  % give us the number of packets each node would try to transmit during the
  % simulation. 
+ Pt      = 2;        % Packet type. Right now packet could be either 
+ % "To Forward" packet, or regular packet which originates from the same
+ % node. We can add Multicast Type or Broadcase type later on if we wish
+ % to. 
  
  HopsT  = zeros(Mnum,NP); % Hops table to generate randomly distributed
  % NP no of hops that the packet would need to make to its destination
  
  % Packet States Enum (Not sure how many we need, just initializing)
  
- Ready  = 0;        % Packet ready to be transmitted
- Trans  = 1;        % Packet Transmission Successful
- Colli  = 2;        % Packet Collision
- Retry  = 3;        % Packet Needs to be retransmitted (State after Colli?)
- Rmax   = 4;        % Packet retry limit reached, drop the packet (RT = 7)
+ Inv    = 0;        % Packet is invalid
+ Ready  = 1;        % Packet ready to be transmitted
+ Trans  = 2;        % Packet Transmission Successful
+ Colli  = 3;        % Packet Collision
+ Retry  = 4;        % Packet Needs to be retransmitted (State after Colli?)
+ Rmax   = 5;        % Packet retry limit reached, drop the packet (RT = 7)
+ 
+ % Packet Type Enum (We may need it if we decide to implement broadcasting)
+ 
+ Normal = 1;        % Normal type packet
+ Fwd    = 2;        % Forwarding type packet
+ BC     = 3;        % Broadcast packet
+ MC     = 4;        % Multicast packet
  
  % Packet Encapsulation (Do we declare a packet by new_pkt = Pkt???)
  % @Des : Final Destination of Packet
  % @Tdes : Destination Node during current transit
  % @Tsrc : Source Node during current transit
  % @Src : Original Source of the packet
+ % @Type : Type of packet - Normal, MC or BC
+ % @State : State of packet
+ % @Rtr : No. of Retries count for the packet
  % @Data : Packet Data
  
- NW_Pkt(Mnum,NP)    = struct('Des', [], 'Tdes', [], 'Tsrc', [], 'Src', [], 'State', [], 'Data', []);
- 
+ % NW Layer just generates 100 packets for each node at the start of the
+ % simulation. As the simulation proceeds, the packets from this queue are
+ % fetched into LL_MCQ and LL_LCQ, which is Link Layer More Capable Queues
+ % and Link Layer Less Capable Queues. 
+ NW_Pkt(Mnum,NP)    = struct('Des', [], 'Tdes', [], 'Tsrc', [], 'Src', [], 'Type', [], 'State', [], 'Rtr', [], 'Data', []);
+ % Link Layer More Capable Queue. Each node would maintain a queue for the
+ % packets which can be forwarded on the More capable link. Further, the
+ % queue would have Pt type subqueues depending upon the packet type.
+ % Forwarding type packets are prefered upon the Local originating packets.
+ LL_MCQ(Mnum,NP,Pt) = struct('Des', [], 'Tdes', [], 'Tsrc', [], 'Src', [], 'Type', [], 'State', [], 'Rtr', [], 'Data', []);
+ % Link Layer Less Capable Queue. Each node would maintain a queue for the
+ LL_LCQ(Mnum,NP,Pt) = struct('Des', [], 'Tdes', [], 'Tsrc', [], 'Src', [], 'Type', [], 'State', [], 'Rtr', [], 'Data', []);
  % Start Topology Simulation
  for idxS = 1:Ns,
      % Get the node distribution, link table and max. no. of hops for each
@@ -77,6 +103,10 @@
      % destination in range [1,mhops(i)]
      for idxNode = 1:Mnum
          HopsT(idxNode,:) = randi([1,mhops(idxNode)],1,NP);
+         Node(idxNode).ID = idxNode;
+         Node(idxNode).iNWQ = 1;
+         Node(idxNode).iMCQ = [1 1];
+         Node(idxNode).iLCQ = [1 1];
          % Construct the NP packets for each node. The packets by ith node
          % need to make hops are stored in ith row of HopsT . Function
          % nodes_with_n_hops(links, x, n) retruns the possible destinations 
@@ -95,12 +125,35 @@
              % Get the routing table and update the next transit
              % destination
              RouteDes = route(idxNode, NW_Pkt(idxNode,idxNP).Des, links ); 
-             NW_Pkt(idxNode,idxNP).Tdes = RouteDes(1);            
+             NW_Pkt(idxNode,idxNP).Tdes = RouteDes(2);
+             NW_Pkt(idxNode,idxNP).Type = Normal;
+             NW_Pkt(idxNode,idxNP).State = Ready; % Ready to transmit
+             NW_Pkt(idxNode,idxNP).Rtr = 0;
+             NW_Pkt(idxNode,idxNP).No = idxNp;
          end %for idxNP = 1:NP
      end % for idxNode = 1:Mnum
      
+     % Pre Transmission packet queue processing. I think it needs to be
+     % done before each Aloha Slot simulation. Will brainstorm and fix the
+     % placement of this code. 
+     
+     for idxNode = 1:Mnum
+         % Fetch the packet from NW queue into one of LL queue
+         idxNextPkt = Node(idxNode).iNWQ;
+         Node(idxNode).iNWQ = idxNextPkt + 1;
+         TempPkt = NW_Pkt(idxNode,idxNextPkt);
+         MoreCap = link(TempPkt.TSrc,TempPkt.Tdes);
+         if MoreCap > 0
+             LL_MCQ(idxNode,Node(idxNode).iMCQ(1,Normal),Normal) = TempPkt;
+             Node(idxNode).iMCQ(1,Normal) = Node(idxNode).iMCQ(1,Normal) + 1; 
+         else
+             LL_LCQ(idxNode,Node(idxNode).iLCQ(1,Normal),Normal) = TempPkt;
+             Node(idxNode).iLCQ(1,Normal) = Node(idxNode).iLCQ(1,Normal) + 1;
+         end
+     end
+     
      % Simulate SLOHA Here: ToDo
-     collisions(Mnum)=0 % initialize collision count for each node
+     collisions(Mnum) = 0 % initialize collision count for each node
      for idxT = 1:Nt
         % Nodes that will attempt to transmit
         for idxNode = 1:Mnum
@@ -112,7 +165,11 @@
         % Evaluate collisions and schedule retransmission slot
         % ceil(2^(num. of collisions)*rand(1)) is the binary exponential
         % backoff delay in # of slots
-        %
+     % Post Transmission Packet Processing
+     % Things ToDo here -----
+        % Update the Tdes fields of forwarding packets here
+        % If Pkt.Des = Pkt.Tdes, destination reached. Increment the End to
+        % End throughput counter. Else, increment Link Throughput counter. 
         % Expire packets that have collided > max retries
      end % for idxT = 1:Nt
      % Performance Graph code goes here
