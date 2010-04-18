2226d85bca9bab94ec68c5c394bd83e15946c934
kareemg <kareemg@39738bbd-366b-da62-1013-27541a91aac4>
2010-04-18 04:23:57 +0000

    added function which calculates and plots the theoretical throughput

--- link_sim.m
+++ link_sim.m
@@ -15,21 +15,31 @@ n = 4;
 
 for y=1:N
     [node, ~, ~] = topo(maxn, maxx, maxy, sigma,45,0);
+
     for x=1:length(theta);
        links = connectivity(node,Dmax,theta(x));
        Ndeg(x) = Ndeg(x)+network_degree(links);
        Rm(x) = Rm(x)+node_ratio(links);
-           dl = Dmax*(cos(theta(x)*pi/180))^(2/n);
-           dm = Dmax*(sin(theta(x)*pi/180))^(2/n);
-           pm(x) = (pi*dm^2)/(maxx*maxy);
+       
+       dl = Dmax*(cos(theta(x)*pi/180))^(2/n);
+       dm = Dmax*(sin(theta(x)*pi/180))^(2/n);
+       pm(x) = (pi*dm^2)/(maxx*maxy);
        Rma(x) = 1 - (1 - pm(x))^(maxn-1);
+       if(y==1)
+       SimThroughput(links, maxn, Dmax, maxx, maxy,theta(x))
+       hold on;
+       end
        %drawfigure(maxx,maxy,node,links,'unit',1)
     end
 end
+hold off
 Ndeg=Ndeg./N;
 Rm=Rm./N;
 figure
 plot(theta,Rma)
+    title('More capable node Ratio v. \theta (Theoretical)');
+    xlabel('Morecapable node Ratio');
+    xlabel('Offset Angle \theta');
 figure
 plot(theta,Ndeg)
     title('Network Degree v. \theta');
