a2110676c2e333dd5e66b78b7e764e19957989d7
kareemg <kareemg@39738bbd-366b-da62-1013-27541a91aac4>
2010-04-18 20:10:45 +0000

    added functions to plot End-to-End Throughputs

---
+++ plote2e.m
@@ -0,0 +1,35 @@
+function [Y] = plote2e( root_dir, Theta )
+%PLOTAVG Summary of this function goes here
+%   Detailed explanation goes here
+x=1;
+Theta = num2str(Theta);
+Xstr = [];
+Ystr = [];
+while (exist([root_dir,'\Theta-',Theta,'\',num2str(x), '.mat'],'file'))
+    S = ['load ',root_dir,'\Theta-',Theta,'\',num2str(x),'.mat X End2EndT'];
+    eval(S);
+    S = ['X',num2str(x),'=X;'];
+    eval(S);
+    S = ['End2EndT',num2str(x),'=End2EndT;'];
+    eval(S);
+    Xstr = [Xstr,'X',num2str(x),' '];
+    Ystr = [Ystr,'Y',num2str(x),' '];
+    x = x + 1;
+end
+%S = ['semilogx([',Xstr,'],[',Ystr,'],',char(39),'kx',char(39),')'];
+%S = ['createfigure([',Xstr,'],[',Ystr,'],Theta)'];
+%eval(S)
+x=x-1;
+for y=1:x
+    S = ['maxy(y) = max(End2EndT',num2str(y),');'];
+    eval(S);
+end
+x = find(maxy == max(maxy));
+S = ['createfigure(X',num2str(x),',End2EndT',num2str(x),',Theta);'];
+eval(S);
+%S = ['X = X',num2str(x),';'];
+eval(S);
+%S = ['Y = End2EndT',num2str(x),';'];
+Y = mean(maxy);
+%eval(S);
+end
